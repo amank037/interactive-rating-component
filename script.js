1f1f8fb3bb9f921b62421ef27da6463d1e9d3287
amank037 <138704088+amank037@users.noreply.github.com>
2024-03-25 12:54:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,16 +6,5 @@ function submitRating(){
     document.querySelector('.rating').style.display = 'none'
 }
 
-const ratingInput = document.querySelectorAll('.rating-input')
 
-ratingInput.forEach(function(rating){
-    rating.addEventListener('click',function(event){
-        event.preventDefault()
-        if(this.classList.contains('active')){
-            this.classList.remove('active')
-        } else { 
-            this.classList.add('active')
-        }
-        
-    })
-})
+
